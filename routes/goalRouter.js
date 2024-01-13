3ff54e0e81f92837860ade3defedb425bce40f24
samantha_nisus_johnson <128253624+SamJohn87@users.noreply.github.com>
2024-01-13 01:24:45 -0600

    setup simple routes

---
+++ routes/goalRouter.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const goalRouter = express.Router();
+
+goalRouter.route('/')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res) => {
+    res.end('Will send all the goals to you');
+})
+.post((req, res) => {
+    res.end(`Will add the goal: ${req.body.title}`);
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /goals');
+})
+.delete((req, res) => {
+    res.end('Deleting all goals');
+});
+
+module.exports = goalRouter;
